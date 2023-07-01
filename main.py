2f3910b16b44d2411d90fa16c354a4d5862f9366
allen <allenkuo07202@gmail.com>
2023-07-01 10:37:29 +0800

    flask_7.1_1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 print(__name__)
@@ -28,4 +28,30 @@ def index():
     return 'Hello Flask123!'
 
 
-app.run(debug=True)
+# app.run(debug=True)
+
+# 2023/7/1   start
+
+def get_today():
+    date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    return date
+
+
+@app.route("/stock")
+def stock():
+    stocks = [
+        {'分類': '日經指數', '指數': '22,920.30'},
+        {'分類': '韓國綜合', '指數': '2,304.59'},
+        {'分類': '香港恆生', '指數': '25,083.71'},
+        {'分類': '上海綜合', '指數': '3,380.68'}
+    ]
+    for stock in stocks:
+        print(stock["分類"], stock["指數"])
+
+    return render_template("stock.html", date=get_today(), stocks=stocks)
+
+
+if __name__ == "__main__":
+    # print(get_bmi(167, 67.5))
+
+    app.run(debug=True)
